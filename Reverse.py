591c012a8eae934b4e70a47e7248550c47a9b363
parkavisivamurugan <parkavisivamurugan@gmail.com>
2022-01-17 20:26:19 +0530

    First Commit

---
+++ Reverse.py
@@ -0,0 +1,13 @@
+def main():
+    print("Enter a string:")
+    a=input()
+    print("Reversed string:",a[::-1])
+    print("Enter a number")
+    b=int(input())
+    r=0
+    while b>0:
+        q=b%10
+        r=r*10+q
+        b=b//10
+    print("Reversed number:",r) 
+main()
